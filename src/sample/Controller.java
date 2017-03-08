4fc94a5e93c81890df9f48b1ae5a8e34f3c86c44
carlos <cestrad2@dtcc.edu>
2017-03-08 15:00:39 -0500

    update

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,21 +3,25 @@ package sample;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
 
-public class Controller {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable{
     @FXML
-    private ComboBox cbSearchOptions;
+    private ComboBox<String> cbSearchOptions;
 
-    private ObservableList<String> options = FXCollections.observableArrayList(
-            "Course Number",
-            "Semester",
-            "Course Lead"
-    );
-
-    
-
-    void initialize(){
-
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        cbSearchOptions.getItems().addAll(
+                "Select one...",
+                "Course Number",
+                "Semester",
+                "Course"
+        );
+        cbSearchOptions.setValue("Select one...");
     }
 }
