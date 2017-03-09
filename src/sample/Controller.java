f1c6eb5ec7c163880968accde261238dedbc3ac4
Carlos Estrada <cestrad2@dtcc.edu>
2017-03-09 01:05:56 -0500

    a few UI changes

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,11 +1,11 @@
 package sample;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TitledPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -14,8 +14,28 @@ public class Controller implements Initializable{
     @FXML
     private ComboBox<String> cbSearchOptions;
 
+    @FXML
+    private Tab homeTab;
+
+    @FXML
+    private Tab searchTab;
+
+    @FXML
+    private Tab loginTab;
+
+    @FXML
+    private TitledPane tpSearchResults;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
+        homeTab.setGraphic(new Label("Home"));
+        searchTab.setGraphic(new Label("Search"));
+        loginTab.setGraphic(new Label("Login"));
+
+        //debug
+        tpSearchResults.setVisible(false);
+
         cbSearchOptions.getItems().addAll(
                 "Select one...",
                 "Course Number",
